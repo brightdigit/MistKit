7f71e07036673e449db79db50400ea661b1297fa
leogdion <leogdion@brightdigit.com>
2020-09-11 14:29:32 -0400

    switching package to `swift-lang` (#7)

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -28,7 +28,7 @@ elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
     
   if [[ $TRAVIS_CPU_ARCH == "arm64" ]]; then
     curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
-    sudo apt-get install swift5
+    sudo apt-get install swift-lang
   else 
     wget https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu${RELEASE_NUM}/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
     tar xzf swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
