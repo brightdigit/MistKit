f7ad4d1a092919b1cbd6cecdd7709bfe43cc5bac
Leo Dion <leogdion@brightdigit.com>
2020-09-30 11:12:00 -0400

    changing to CFRunLoopRun #63

--- Sources/mistdemoc/Commands/ParsableAsyncCommand.swift
+++ Sources/mistdemoc/Commands/ParsableAsyncCommand.swift
@@ -7,17 +7,15 @@ protocol ParsableAsyncCommand: ParsableCommand {
 
 extension ParsableAsyncCommand {
   func run() throws {
-    var result: Result<Void, Error>?
+    var result: Result<Void, Error>!
 
     runAsync { error in
       result = Result(error)
+      CFRunLoopStop(CFRunLoopGetMain())
     }
 
-    while true {
-      RunLoop.main.run(until: .distantPast)
-      if let result = result {
-        return try result.get()
-      }
-    }
+    CFRunLoopRun()
+
+    return try result.get()
   }
 }
