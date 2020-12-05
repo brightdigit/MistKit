8b6a1bd15e6c0a38fd72c1f229442e0983bad3fe
Leo Dion <leogdion@brightdigit.com>
2020-12-04 19:22:15 -0500

    created different images

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-# Dockerfile
-FROM swift:5.3
-RUN apt-get update
-RUN apt-get install libsqlite3-dev
---
+++ Scripts/images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+swift_versions=('5.3' '5.2')
+ubuntu_versions=('bionic' 'xenial' 'focal')
+
+#docker build -t brightdigit/mistkit-sql .
+#docker push brightdigit/mistkit-sql
+
+for swift_version in ${swift_versions[@]}
+do
+    for ubuntu_version in ${ubuntu_versions[@]}
+    do
+      #echo $swift_version-$ubuntu_version
+      (
+      docker build -t brightdigit/mistkit-sql:$swift_version-$ubuntu_version . --build-arg TAG=$swift_version-$ubuntu_version
+      docker push brightdigit/mistkit-sql:$swift_version-$ubuntu_version
+      ) &
+    done
+done
+wait
