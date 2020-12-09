e0bc7f418916504aafa3e16c1c0f1a18cec5b651
Leo Dion <leogdion@brightdigit.com>
2020-12-08 21:13:02 -0500

    fixing linting issues

--- Sources/MistKit/Configuration/MKDatabaseConnection.Query.swift
+++ Sources/MistKit/Configuration/MKDatabaseConnection.Query.swift
@@ -1,3 +1,4 @@
+// swiftlint:disable:this file_name
 import Foundation
 
 public extension MKDatabaseConnection {
--- Sources/MistKit/MKDatabase.URLSession.swift
+++ Sources/MistKit/MKDatabase.URLSession.swift
@@ -1,3 +1,5 @@
+// swiftlint:disable:this file_name
+
 import Foundation
 
 #if canImport(FoundationNetworking)
