dde739648df26584378eb77c0d281d148d93740b
Leo Dion <leogdion@brightdigit.com>
2021-01-27 17:19:13 -0500

    fixing ci and adding tests

--- Tests/MistKitTests/Extensions/ArrayTests.swift
+++ Tests/MistKitTests/Extensions/ArrayTests.swift
@@ -1,3 +1,20 @@
 @testable import MistKit
 import XCTest
-final class ArrayTests: XCTestCase {}
+final class ArrayTests: XCTestCase {
+  func testUUID() {
+    let count = 40
+    let expectedUUIDs = (1 ... count).map { _ in
+      UUID()
+    }
+    let arrays = expectedUUIDs.map { uuid -> [UInt8] in
+      let array = Array(uuid: uuid)
+      XCTAssertEqual(array.count, 16)
+      return array
+    }
+    let actualUUIDs = arrays.map { array -> UUID in
+      UUID(data: Data(array))
+    }
+
+    XCTAssertEqual(expectedUUIDs, actualUUIDs)
+  }
+}
--- Tests/MistKitTests/Extensions/UUIDTests.swift
+++ Tests/MistKitTests/Extensions/UUIDTests.swift
@@ -1,3 +1,18 @@
 @testable import MistKit
 import XCTest
-final class UUIDTests: XCTestCase {}
+final class UUIDTests: XCTestCase {
+  func testArray() {
+    let count = 40
+    let uuidByteSize = 16
+    let expectedDatas = (1 ... count).map { _ in
+      (1 ... uuidByteSize).map { _ in
+        UInt8.random(in: 0 ... 255)
+      }
+    }.map(Data.init(_:))
+    let uuids = expectedDatas.map(UUID.init(data:))
+    let actualDatas = uuids.map(Array.init(uuid:)).map(Data.init(_:))
+    let actualDataProps = uuids.map { $0.data }.map { $0 as Data }
+    XCTAssertEqual(expectedDatas, actualDatas)
+    XCTAssertEqual(expectedDatas, actualDataProps)
+  }
+}
