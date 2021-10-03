0fb5e7b86133588168e04fed7715ed750d76ca66
Leo Dion <leogdion@brightdigit.com>
2021-10-03 17:33:44 -0400

    Version 0.2.4

--- Package.swift
+++ Package.swift
@@ -35,13 +35,13 @@ let package = Package(
     .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
     .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0"),
-    // dev
-    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.1"), // dev
-    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.12"), // dev
-    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
-    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
-    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
+//    // dev
+//    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.1"), // dev
+//    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
+//    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.12"), // dev
+//    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
+//    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
+//    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
