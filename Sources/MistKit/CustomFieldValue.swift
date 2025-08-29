5c77f83ddc4cac6ed32b72bf5d8d9ad6c34d9758
Leo Dion <leogdion@brightdigit.com>
2025-08-29 08:44:47 -0400

    fixup! feat: add RecordFieldConverter for CloudKit field mapping

--- Sources/MistKit/CustomFieldValue.swift
+++ Sources/MistKit/CustomFieldValue.swift
@@ -65,6 +65,29 @@ internal struct CustomFieldValue: Codable, Hashable, Sendable {
     case type
   }
 
+  private static let fieldTypeDecoders:
+    [FieldTypePayload: (KeyedDecodingContainer<CodingKeys>) throws -> CustomFieldValuePayload] = [
+      .string: { .stringValue(try $0.decode(String.self, forKey: .value)) },
+      .int64: { .int64Value(try $0.decode(Int64.self, forKey: .value)) },
+      .double: { .doubleValue(try $0.decode(Double.self, forKey: .value)) },
+      .bytes: { .bytesValue(try $0.decode(String.self, forKey: .value)) },
+      .reference: {
+        .referenceValue(try $0.decode(Components.Schemas.ReferenceValue.self, forKey: .value))
+      },
+      .asset: { .assetValue(try $0.decode(Components.Schemas.AssetValue.self, forKey: .value)) },
+      .assetid: { .assetValue(try $0.decode(Components.Schemas.AssetValue.self, forKey: .value)) },
+      .location: {
+        .locationValue(try $0.decode(Components.Schemas.LocationValue.self, forKey: .value))
+      },
+      .timestamp: { .dateValue(try $0.decode(Double.self, forKey: .value)) },
+      .list: { .listValue(try $0.decode([CustomFieldValuePayload].self, forKey: .value)) },
+    ]
+
+  private static let defaultDecoder:
+    (KeyedDecodingContainer<CodingKeys>) throws -> CustomFieldValuePayload = {
+      .stringValue(try $0.decode(String.self, forKey: .value))
+    }
+
   /// The field value payload
   internal let value: CustomFieldValuePayload
   /// The field type
@@ -146,39 +169,15 @@ internal struct CustomFieldValue: Codable, Hashable, Sendable {
     from container: KeyedDecodingContainer<CodingKeys>,
     type fieldType: FieldTypePayload
   ) throws -> CustomFieldValuePayload {
-    let decoder = fieldTypeDecoders[fieldType] ?? fieldTypeDecoders[.string]!
+    let decoder = fieldTypeDecoders[fieldType] ?? defaultDecoder
     return try decoder(container)
   }
-
-  private static let fieldTypeDecoders:
-    [FieldTypePayload: (KeyedDecodingContainer<CodingKeys>) throws -> CustomFieldValuePayload] = [
-      .string: { .stringValue(try $0.decode(String.self, forKey: .value)) },
-      .int64: { .int64Value(try $0.decode(Int64.self, forKey: .value)) },
-      .double: { .doubleValue(try $0.decode(Double.self, forKey: .value)) },
-      .bytes: { .bytesValue(try $0.decode(String.self, forKey: .value)) },
-      .reference: {
-        .referenceValue(try $0.decode(Components.Schemas.ReferenceValue.self, forKey: .value))
-      },
-      .asset: { .assetValue(try $0.decode(Components.Schemas.AssetValue.self, forKey: .value)) },
-      .assetid: { .assetValue(try $0.decode(Components.Schemas.AssetValue.self, forKey: .value)) },
-      .location: {
-        .locationValue(try $0.decode(Components.Schemas.LocationValue.self, forKey: .value))
-      },
-      .timestamp: { .dateValue(try $0.decode(Double.self, forKey: .value)) },
-      .list: { .listValue(try $0.decode([CustomFieldValuePayload].self, forKey: .value)) },
-    ]
 
   private static func decodeFallbackValue(
     from container: KeyedDecodingContainer<CodingKeys>
   ) throws -> CustomFieldValuePayload {
     let valueContainer = try container.superDecoder(forKey: .value)
     return try CustomFieldValuePayload(from: valueContainer)
-  }
-
-  internal func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encodeIfPresent(type, forKey: .type)
-    try Self.encodeValue(value, to: &container)
   }
 
   private static func encodeValue(
@@ -197,5 +196,11 @@ internal struct CustomFieldValue: Codable, Hashable, Sendable {
     case .assetValue(let val): try container.encode(val, forKey: .value)
     case .listValue(let val): try container.encode(val, forKey: .value)
     }
+  }
+
+  internal func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encodeIfPresent(type, forKey: .type)
+    try Self.encodeValue(value, to: &container)
   }
 }
